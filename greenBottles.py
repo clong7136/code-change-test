f2034974fe174375901dd0f83b8018e94d5617ca
Ymir_Is_Here <clong7136@sohamcollege.org.uk>
2014-05-15 17:13:57 +0100

    Updated
    used worded numbers rather than digits.

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,25 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+number = [
+'Zero',
+'One',
+'Two',
+'Three',
+'Four',
+'Five',
+'Six',
+'Seven',
+'Eight',
+'Nine',
+'Ten' ]
+
+text_one = 'green bottles hanging on the wall'
+text_two = 'And if one green bottle should accidentally fall\nThere\'ll be'
+
+# Each part of the loop makes a new verse
+for i in range(10, 0, -1):
+    print(number[i], text_one)
+    time.sleep(1)
+    print(number[i], text_one)
+    time.sleep(1)
+    print(text_two, number[i-1], text_one)
+    time.sleep(1)
